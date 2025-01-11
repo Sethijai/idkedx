3a82f70e48e7a32768c9bdec11a67136ea4cfc6f
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-01-11 12:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def health_check():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.ge
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
